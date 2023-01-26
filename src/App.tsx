f74afa29422eec26345f9af31ef53882430c6111
SergBily <89011743+SergBily@users.noreply.github.com>
2023-01-26 14:10:40 +0300

    init: start mail app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { Login, Mail, Registration } from './pages';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="wrapper">
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="registration" element={<Registration />} />
+      <Route path="mail" element={<Mail />} />
+    </Routes>
+    <ToastContainer />
+  </div>
+);
 
 export default App;
